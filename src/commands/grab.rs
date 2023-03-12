07d8e19e7d8f42c943f63c7acaac6a4e25dc6753
samgozman <sam@gozman.space>
2023-03-12 14:58:17 +0400

    Add unit tests for grab

--- src/commands/grab.rs
+++ src/commands/grab.rs
@@ -1,7 +1,7 @@
-use anyhow::{Result};
+use anyhow::Result;
 use clap::Parser;
 
-use validator::{Validate};
+use validator::Validate;
 
 /// Simply grab one value from a web page.
 #[derive(Parser, Validate)]
@@ -13,7 +13,7 @@ pub struct Args {
     ///
     /// Example: `-s="#search > div"`
     #[arg(short, long, value_name = "PATH")]
-    #[validate(length(min = 1, message="should not be empty!"))]
+    #[validate(length(min = 1, message = "should not be empty!"))]
     selector: String,
 
     /// URL to web page to grab from.
@@ -31,4 +31,38 @@ pub async fn command(args: Args) -> Result<()> {
     println!("Selector: {}", args.selector);
     println!("From: {}", args.from);
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_command() -> Result<()> {
+        let args = Args {
+            selector: "#search > div".to_string(),
+            from: "https://example.com".to_string(),
+        };
+        command(args).await
+    }
+
+    #[tokio::test]
+    async fn test_command_with_invalid_url() -> Result<()> {
+        let args = Args {
+            selector: "#search > div".to_string(),
+            from: "invalid-url".to_string(),
+        };
+        command(args).await.expect_err("should fail with invalid URL!");
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_command_with_empty_selector() -> Result<()> {
+        let args = Args {
+            selector: "".to_string(),
+            from: "https://example.com".to_string(),
+        };
+        command(args).await.expect_err("should fail with empty selector!");
+        Ok(())
+    }
 }
