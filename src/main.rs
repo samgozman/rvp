505caa834feb9488a8b58bb10336d551f3bed71d
samgozman <sam@gozman.space>
2023-03-12 15:59:14 +0400

    Remove default options

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,11 @@ mod macros;
 use anyhow::Result;
 
 use clap::{Parser, Subcommand};
-use std::path::PathBuf;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
-    /// Optional name to operate on
-    name: Option<String>,
-
-    /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE")]
-    config: Option<PathBuf>,
-
+    // Specify optional parameters here, before the subcommand
     #[command(subcommand)]
     command: Commands,
 }
@@ -28,15 +21,6 @@ commands_builder!(grab);
 #[tokio::main]
 async fn main() -> Result<()> {
     let cli = Cli::parse();
-
-    if let Some(name) = cli.name.as_deref() {
-        println!("Value for name: {name}");
-    }
-
-    if let Some(config_path) = cli.config.as_deref() {
-        println!("Value for config: {}", config_path.display());
-    }
-
     Commands::exec(cli).await?;
     Ok(())
 }
