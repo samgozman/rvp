382f812ebd40f0fe7ea8e649d17fa3f47d9823fc
samgozman <sam@gozman.space>
2023-03-12 14:21:12 +0400

    Add validator

--- src/commands/grab.rs
+++ src/commands/grab.rs
@@ -1,8 +1,10 @@
-use anyhow::Result;
+use anyhow::{Result};
 use clap::Parser;
 
+use validator::{Validate};
+
 /// Simply grab one value from a web page.
-#[derive(Parser)]
+#[derive(Parser, Validate)]
 pub struct Args {
     /// Selector path to grab from the page.
     ///
@@ -11,16 +13,20 @@ pub struct Args {
     ///
     /// Example: `-s="#search > div"`
     #[arg(short, long, value_name = "PATH")]
+    #[validate(length(min = 1, message="should not be empty!"))]
     selector: String,
 
     /// URL to web page to grab from.
     ///
     /// Example: `-f="https://example.com"`
     #[arg(short, long, value_name = "URL")]
+    #[validate(url(message = "must be a valid URL!"))]
     from: String,
 }
 
 pub async fn command(args: Args) -> Result<()> {
+    args.validate()?;
+
     println!("Grab command");
     println!("Selector: {}", args.selector);
     println!("From: {}", args.from);
