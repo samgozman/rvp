1d3fa84db606646025787cc3d64a7a279584c6c1
samgozman <sam@gozman.space>
2023-04-12 10:31:09 +0400

    Add option to pass one param for each resource

--- src/commands/batch.rs
+++ src/commands/batch.rs
@@ -35,6 +35,19 @@ pub struct Args {
     #[arg(long, num_args(0..))]
     params: Option<Vec<String>>,
 
+    /// (Optional) Single parameter to be passed to all resources.
+    /// This argument is mutually exclusive with `params`.
+    ///
+    /// Example:
+    ///
+    /// ```
+    /// --one-param param1
+    /// ```
+    ///
+    /// This argument is useful when you want to pass the same parameter to all resources.
+    #[arg(long, conflicts_with = "params")]
+    one_param: Option<String>,
+
     /// Output the data in JSON format
     #[arg(long)]
     json: bool,
@@ -54,22 +67,32 @@ pub async fn command(args: Args) -> Result<()> {
     let mut config = Config::from_file(&args.path, &config_format)?;
 
     if config.needs_parameters() {
-        if args.params.is_none() {
+        if args.params.is_none() && args.one_param.is_none() {
             return Err(anyhow!(
                 "This config needs parameters!\nMore info: rvp batch --help"
             ));
         }
 
-        let params = args.params.unwrap();
+        if args.params.is_some() {
+            let params = args.params.unwrap();
 
-        if config.resources.len() != params.len() {
-            return Err(anyhow!(
-                "The number of parameters does not match the number of resources!"
-            ));
+            if config.resources.len() != params.len() {
+                return Err(anyhow!(
+                    "The number of parameters does not match the number of resources!"
+                ));
+            }
+
+            for (i, param) in params.iter().enumerate() {
+                config.resources[i].mut_url_with_param(param);
+            }
         }
 
-        for (i, param) in params.iter().enumerate() {
-            config.resources[i].mut_url_with_param(param);
+        if args.one_param.is_some() {
+            let param = args.one_param.unwrap();
+
+            for resource in config.resources.iter_mut() {
+                resource.mut_url_with_param(&param);
+            }
         }
     }
 
